513f7c771571a6bbd6c5a2cf80638334151b9783
Ngọc Sơn 0812 <91933401+ngocson812@users.noreply.github.com>
2022-02-08 15:57:34 +0700

    Add files via upload

---
+++ casemd4/src/main/java/com/example/casemd4/Casemd4Application.java
@@ -0,0 +1,13 @@
+package com.example.casemd4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Casemd4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Casemd4Application.class, args);
+	}
+
+}
